8a1c3e8878ab78496c41f8dc0299853c36067da1
AL <aleks.ustya@mail.ru>
2025-03-27 18:41:27 +0300

    Update Next.js config for production build

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
     domains: ['picsum.photos'], // Разрешаем загрузку изображений с picsum.photos
     unoptimized: true
