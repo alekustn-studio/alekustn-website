32734ee551d422bf2577446b474ea4eb048b3adb
AL <aleks.ustya@mail.ru>
2025-03-27 20:34:00 +0300

    Simplify Next.js config

--- next.config.js
+++ next.config.js
@@ -3,26 +3,7 @@ const nextConfig = {
   images: {
     domains: ['picsum.photos'],
     unoptimized: true
-  },
-  experimental: {
-    appDir: true
-  },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.css$/,
-      use: [
-        'style-loader',
-        {
-          loader: 'css-loader',
-          options: {
-            importLoaders: 1,
-          },
-        },
-        'postcss-loader',
-      ],
-    });
-    return config;
-  },
+  }
 }
 
 module.exports = nextConfig
