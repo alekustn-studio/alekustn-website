1f24f04f42dd0cacedb50f083dd00c443328e098
AL <aleks.ustya@mail.ru>
2025-03-27 20:43:45 +0300

    Simplify PostCSS config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   images: {
     domains: ['picsum.photos'],
     unoptimized: true
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader', 'postcss-loader']
+    });
+    return config;
   }
 }
 
