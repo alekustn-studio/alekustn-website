e864708280f90124f8d89fd8144f97abe727ddd3
AL <aleks.ustya@mail.ru>
2025-03-28 21:58:30 +0300

    Return social links and copyright to original position

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -72,7 +72,7 @@ export default function Menu({ onClose, isOpen }: MenuProps) {
 
       <div style={{
         position: 'absolute',
-        bottom: '110px',
+        bottom: '32px',
         left: '32px',
         width: 'calc(100% - 64px)'
       }}>
