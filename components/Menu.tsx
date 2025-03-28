fc864ee499aa5d7046884fac1a1e7df2af21bf06
AL <aleks.ustya@mail.ru>
2025-03-28 22:10:51 +0300

    Fix all menu elements positioning

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -72,7 +72,7 @@ export default function Menu({ onClose, isOpen }: MenuProps) {
 
       <div style={{
         position: 'absolute',
-        bottom: '80px',
+        bottom: '140px',
         left: '32px',
         width: 'calc(100% - 64px)'
       }}>
